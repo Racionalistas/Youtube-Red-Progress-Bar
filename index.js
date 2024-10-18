0d91c925292f15f4a9571e5b7ac8b992118e707e
Александр Приходько <108543198+Racionalistas@users.noreply.github.com>
2024-10-18 18:53:14 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-setTimeout(() => document.querySelectorAll('.ytp-play-progress').forEach((elem) => elem.style.background = '#f03'));
+let iterationsToUpd = 0;
+
+const checkElements = setInterval(() => {
+    const elements = document.querySelectorAll('.ytp-play-progress');
+
+    if (elements.length > 0) {
+        if (elements.length == 1 && iterationsToUpd < 3) {
+            iterationsToUpd++;
+            return;
+        }
+
+        elements.forEach((elem) => {
+            elem.style.background = '#f03';
+        });
+
+        clearInterval(checkElements);
+    }
+}, 250)
