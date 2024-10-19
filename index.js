7fdebe0ad5c8d3519d798a393f46fffe928f3f51
Александр Приходько <108543198+Racionalistas@users.noreply.github.com>
2024-10-19 12:06:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ let iterationsToUpd = 0;
 
 const checkElements = setInterval(() => {
     const elements = document.querySelectorAll('.ytp-play-progress');
+    const playbackElm = document.querySelectorAll('.ytd-thumbnail-overlay-resume-playback-renderer');
+    const progressBarElm = document.querySelectorAll('.yt-page-navigation-progress');
 
-    if (elements.length > 0) {
-        if (elements.length == 1 && iterationsToUpd < 3) {
-            iterationsToUpd++;
-            return;
-        }
-
-        elements.forEach((elem) => {
-            elem.style.background = '#f03';
-        });
-
-        clearInterval(checkElements);
+    if (elements.length < 2 && iterationsToUpd < 3) {
+        iterationsToUpd++;
+        return;
     }
+
+    elements.forEach((elem) => {
+        elem.style.background = '#f03';
+    });
+
+    playbackElm.forEach((elem) => {
+        elem.style.background = '#f03';
+    });
+
+    progressBarElm.forEach((elem) => {
+        elem.style.background = '#f03';
+    });
+
+    clearInterval(checkElements);
 }, 250)
